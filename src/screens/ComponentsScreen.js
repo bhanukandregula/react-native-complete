ce8b5ebf4d341bd2c1215a9e2aa62b1968c06313
Bhanu Kandregula <bkandreg@bhanukandregula.com>
2020-06-24 23:54:17 -0400

    Second commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,30 @@
+// creating a react component here, which returns jsx
+// jsx looks like a html, for understanding
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ComponentsScreen = () => {
+  // we can only show const value in the JSX, but not the Java Script object - FYI
+  const greetings = "Good morning";
+
+  // or we can create assign the react value to const and render in JSX
+  const wishes = <Text> Its a great day!! Have fun </Text>;
+
+  // style will also work with: style={{ fontSize: 30 }}
+  return (
+    <View>
+      <Text style={styles.textStyle}>Ji Shri Ram</Text>
+      <Text style={styles.textStyle}>Ji Bhajarangabali</Text>
+      <Text>{greetings}</Text>
+      {wishes}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 18,
+  },
+});
+
+export default ComponentsScreen;
