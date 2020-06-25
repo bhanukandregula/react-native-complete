31bce5bdb6013bb633b4a18cd0b263c9e6916c98
Bhanu Kandregula <bkandreg@bhanukandregula.com>
2020-06-25 12:04:03 -0400

    state management of color square with swithc case

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,12 +10,22 @@ const SquareScreen = () => {
   const [blue, setBlue] = useState(0);
 
   const setColor = (color, change) => {
-    // color = red, green or blue.
-    // change = +15 or -15
-    if (red + change > 255 || red + change < 0) {
-      return;
-    } else {
-      setRed(red + change);
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+
+      default:
+        return;
     }
   };
 
